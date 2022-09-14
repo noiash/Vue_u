8308ed8e329ee54cc0a6cc0b7df839de23c0bd49
noiash <noinoi51267@gmail.com>
2022-09-14 21:25:58 +0900

    [Add]section8

---
+++ section8/bookapp/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-app>
+  <HeaderPage/>
+    <v-main>
+      <router-view/>
+    </v-main>
+  <FooterPage/>
+  </v-app>
+</template>
+
+<script>
+import FooterPage from './global/FooterPage.vue';
+import HeaderPage from './global/HeaderPage.vue';
+
+export default {
+    name: "App",
+    data: () => ({
+    //
+    }),
+    components: { FooterPage, HeaderPage }
+};
+</script>
