6f13fce84e5a3a78044a42e60e54214760289ffe
noiash <noinoi51267@gmail.com>
2022-09-14 16:02:31 +0900

    [Add]section7

--- section7/vuerouter/src/App.vue
+++ section7/vuerouter/src/App.vue
@@ -3,7 +3,9 @@
     <nav>
       <router-link to="/">Home</router-link> |
       <!-- <router-link to="/about" tag="button">About</router-link> -->
-      <router-link to="/about" exact-active-class="test">About</router-link>
+      <router-link to="/about" exact-active-class="test">About</router-link> |
+      <router-link to="/book"> BookList</router-link> |
+      <router-link to="/item/1"> Item</router-link>
     </nav>
     <router-view/>
   </div>
--- section7/vuerouter/src/router/index.js
+++ section7/vuerouter/src/router/index.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import BookList from '../views/BookList.vue'
+import BookDetail from '@/components/BookDetail.vue'
+import ItemPage from '../views/ItemPage.vue'
+import NotFound from '@/components/NotFound.vue'
 
 Vue.use(VueRouter)
 
@@ -17,6 +21,32 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/book',
+    name: 'BookList',
+    component: BookList
+  },
+  {
+    path: '/book/:id',
+    name: 'Book',
+    component: BookDetail,
+    props: route => ({ 
+      id: Number(route.params.id),
+      title: route.params.title,
+      content: route.params.content
+    })
+  },
+  {
+    path: '/item/:id',
+    name: 'Item',
+    component: ItemPage
+  },
+  {
+    path: '*',
+    // redirect: '/'
+    name: 'NotFound',
+    component: NotFound
   }
 ]
 
