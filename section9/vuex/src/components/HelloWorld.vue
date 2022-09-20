247f6a0f37c8a7cfd3200bd808d784f91bab88c9
noiash <noinoi51267@gmail.com>
2022-09-20 09:41:15 +0900

    [Start]section9

---
+++ section9/vuex/src/components/HelloWorld.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <button @click="increment">+</button>
+    <button @click="addCount">+10</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  //methods: {
+    //increment(){
+      //this.$store.commit('increment')
+    //},
+    //addCount() {
+      //this.$store.commit('addCount', {
+        //value: 10
+      //})
+    //}
+  //}
+  increment(){
+    this.$store.dispatch('incrementAction')
+  },
+  addCount(){
+    this.$store.dispatch('addCountAction', {
+      value: 10
+    })
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ section9/vuex/src/store/index.js
@@ -0,0 +1,36 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: { //初期値
+    count: 0,
+
+  },
+  getters: {
+  },
+  mutations: {
+    increment(state) {
+      state.count++
+    },
+    addCount(state, payload) { //第二引数はオブジェクト
+      state.count += payload.value
+    }
+  },
+  actions: {
+    // incrementAction(context) {
+    //   context.commit('increment')
+    // },
+    incrementAction({ commit }) {
+      commit('increment')
+    },
+    addCountAction({ commit }, payload) {
+      commit('addCount', payload)
+    }
+  },
+  // getters: {
+  // },
+  modules: {
+  }
+})
