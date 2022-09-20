48d077c47d3fe6d675686f01025bb48a8f14a452
noiash <noinoi51267@gmail.com>
2022-09-20 11:53:49 +0900

    [Add]section10

---
+++ section10/vue3-test/src/components/GrandChildren.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>GrandChildren(孫)</div>
+  <div>{{ userName }}</div>
+</template>
+
+<script>
+  export default {
+    data(){},
+    inject: ['userName']
+  }
+</script>
+
+<style>
+
+</style>
