0d02f506b7dce99f10fdffc4bf85c39242f1c80a
noiash <noinoi51267@gmail.com>
2022-09-21 10:17:23 +0900

    [Add]section10

--- section10/vue3-test/src/views/CompositionTest.vue
+++ section10/vue3-test/src/views/CompositionTest.vue
@@ -6,11 +6,18 @@
    <p>ref: {{ nameRef }}</p>
    <p>reactive: {{ book.title }}</p>
    <p>reactive: {{ book.author[0] }}</p>
+   <p>reactiveToRefs: {{ titleRef }}</p>
+   <p>reactiveToRefs: {{ authorRef[1] }}</p>
+   <button @click="btnClick">クリック</button>
+   <p>computed: {{ totalPrice }}</p>
+   <div>watch: <input v-model="search"></div>
+   <div>watchEffect: <input v-model="searchEffect"></div>
   </div>
 </template>
 
 <script>
-import { ref, reactive } from 'vue'
+import { ref, reactive, toRefs,
+ computed, watch, watchEffect, onMounted } from 'vue'
   export default {
     setup(){
       let name = '大谷'
@@ -22,6 +29,41 @@ import { ref, reactive } from 'vue'
         author: ['大谷', '伊藤']
       })
 
+      const booktoRefs = reactive({
+        titleRef: 'タイトル2',
+        authorRef: ['大谷2', '伊藤2']
+      })
+
+      const item = reactive({
+        price: 100,
+        number: 1
+      })
+
+      const totalPrice = computed(() => {
+        return item.price * item.number
+      })
+
+      const btnClick = e => {
+        console.log(book.title)
+        console.log(e)
+      }
+
+      const search = ref('')
+      watch(search, (newValue, prevValue) => {
+        console.log(`watch: ${search.value}`)
+        console.log(`new: ${newValue}`)
+        console.log(`prev: ${prevValue}`)
+      })
+
+      const searchEffect = ref('')
+      watchEffect(() => {
+        console.log(`watchEffect: ${searchEffect.value}`)
+      })
+
+      onMounted(() => {
+        console.log('onMounted')
+      })
+
       console.log('setup')
       console.log(this)
       console.log(nameRef)
@@ -30,7 +72,13 @@ import { ref, reactive } from 'vue'
         name,
         age,
         nameRef,
-        book
+        book,
+        ...toRefs(booktoRefs),
+        btnClick,
+        item,
+        totalPrice,
+        search,
+        searchEffect
       }
     },
     data(){
